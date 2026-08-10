42c73799e34e1893de730cc579e3fee57e1d4d10
Bárbara <barbaragrst9@gmail.com>
2026-08-10 09:33:38 -0400

    primeiro componente

--- imagelite/app/components/PrimeiroComponente.tsx
+++ imagelite/app/components/PrimeiroComponente.tsx
@@ -1,7 +1,15 @@
+'use client';
 export function PrimeiroComponente(){
-  return(
+  
+  function clique(){
+  //  console.log("Você clicou no botão!");
+  alert("Você clicou no botão!");
+  }
+
+    return(
     <div> 
       <h1>Primeiro Componente</h1>
+      <button onClick={clique}>Clique aqui</button>
     </div>
   );
 } 
