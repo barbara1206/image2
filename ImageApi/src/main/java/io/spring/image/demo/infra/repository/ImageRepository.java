913a1abeccb8c1574fdf8a9246ab238e05aaa91d
barbara1206 <barbaragrst9@gmail.com>
2026-09-11 12:14:48 -0400

    11/09/26

---
+++ ImageApi/src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
@@ -0,0 +1,29 @@
+package io.spring.image.demo.infra.repository;
+
+import io.spring.image.demo.domain.entity.Image;
+import io.spring.image.demo.domain.enums.ImageExtension;
+import io.spring.image.demo.infra.repository.specs.GenericSpecs;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static io.spring.image.demo.infra.repository.specs.GenericSpecs.*;
+import static io.spring.image.demo.infra.repository.specs.ImageSpecs.*;
+import static org.springframework.data.jpa.domain.Specification.*;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        Specification<Image> spec = where(conjunction());
+        if(extension !=null){
+            spec = spec.and(extensionEqual(extension));
+        }
+        if(StringUtils.hasText(query)){
+            spec = spec.and(anyOf(nameLike(query), tagsLike(query)));
+        }
+        return findAll(spec);
+    }
+}
